c1797ba622733a2835291e9a3efb6544440922ab
Rúben Martins <91032103+RubenABM@users.noreply.github.com>
2021-10-23 23:02:19 +0100

    Add files via upload

---
+++ POO/myiade/src/test/java/pt/iade/myiade/MyiadeApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.myiade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyiadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
