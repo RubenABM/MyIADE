15eec747cdf0429c1a8a4ee955071f0ac55dac8e
Africano19 <81010248+Africano19@users.noreply.github.com>
2021-10-28 12:51:04 +0100

    poo

--- POO/myiade/src/main/java/pt/iade/myiade/controllers/IadeController.java
+++ POO/myiade/src/main/java/pt/iade/myiade/controllers/IadeController.java
@@ -1,19 +1,28 @@
 package pt.iade.myiade.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import pt.iade.myiade.models.Iade;
 import pt.iade.myiade.models.repositories.IadeRepository;
 
+import org.springframework.http.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
-@RequestMapping(path = "/api/pathexemplo")
+@RequestMapping(path = "/api/Iade")
 public class IadeController
 {
     private Logger logger = LoggerFactory.getLogger(IadeController.class);
     @Autowired
-    private IadeRepository CourseRepository;
+    private IadeRepository iadeRepository;
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Iade> getID() {
+    logger.info("Sending all units");
+    return iadeRepository.findAll();
+    }
+
 }
