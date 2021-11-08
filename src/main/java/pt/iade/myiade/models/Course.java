d81b66b001bc7fbfb776a643108b53fb62208fdd
RubenABM <rubenbatistamartins@gmail.com>
2021-11-08 21:13:58 +0000

    classes

--- src/main/java/pt/iade/myiade/models/Course.java
+++ src/main/java/pt/iade/myiade/models/Course.java
@@ -1,47 +1,20 @@
 package pt.iade.myiade.models;
 
-import java.util.ArrayList;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+@Entity
+@Table(name="Course")
 public class Course {
-    private int id;
-    private String[] unit;
-    private String name;
-
-    @JsonIgnoreProperties({"course"})
-    private ArrayList<Enrolment> enrolments;
-    
-    public Course(int id, String[] unit, String name) {
-        this.id = id;
-        this.unit = unit;
-        this.name = name;
-        enrolments = new ArrayList<Enrolment>();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String[] getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String[] unit) {
-        this.unit = unit;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="cour_id") private int id;
+    @Column(name="cour_name") private String name;
+    public Course() {}
+    public int getId() { return id; }
+    public String getName() { return name; }
 }
