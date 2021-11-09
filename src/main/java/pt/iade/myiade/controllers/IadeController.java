fab39c81d5ec3578a194b0b13f2c2f073e0beb79
RubenABM <rubenbatistamartins@gmail.com>
2021-11-09 20:07:09 +0000

    hhh

--- src/main/java/pt/iade/myiade/controllers/IadeController.java
+++ src/main/java/pt/iade/myiade/controllers/IadeController.java
@@ -20,7 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RestController
-@RequestMapping(path = "/api/testeiade")
+@RequestMapping(path = "/api/teste")
 public class IadeController{
     
     private Logger logger = LoggerFactory.getLogger(IadeController.class);
