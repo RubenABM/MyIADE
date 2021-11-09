83587225b75ab47085540b18e6db5258ec13fbc7
RubenABM <rubenbatistamartins@gmail.com>
2021-11-09 19:31:11 +0000

    aaa

--- src/main/java/pt/iade/myiade/models/repositories/StudentRepository.java
+++ src/main/java/pt/iade/myiade/models/repositories/StudentRepository.java
@@ -1,10 +1,10 @@
 package pt.iade.myiade.models.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
 import pt.iade.myiade.models.Student;
 
-public interface StudentRepository extends CrudRepository<Student,Integer>
+public interface StudentRepository extends CrudRepository<Student,Integer> 
 {
     
 }
+
