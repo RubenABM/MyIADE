fa86ada66804659eb9415dd2ad25374457fb81d2
RubenABM <rubenbatistamartins@gmail.com>
2021-11-09 20:06:01 +0000

    2222

--- src/main/java/pt/iade/myiade/models/Iade.java
+++ src/main/java/pt/iade/myiade/models/Iade.java
@@ -1,5 +1,7 @@
 package pt.iade.myiade.models;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,7 +16,15 @@ public class Iade {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="stu_id") private int id;
     @Column(name="stu_name") private String name;
+    @Column(name="stu_bDate") private LocalDate bDate;
+    @Column(name="stu_phone") private int phone;
     public Iade() {}
     public int getId() { return id; }
     public String getName() { return name; }
+    public LocalDate getBDate() { 
+        return bDate; 
+    }
+    public int getPhone() {   
+        return phone;
+    }
 }
