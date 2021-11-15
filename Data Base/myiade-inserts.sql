b0688911a46336c147926ce848cae40d3db8e47f
Rúben Martins <91032103+RubenABM@users.noreply.github.com>
2021-11-15 11:43:07 +0000

    Update myiade-inserts.sql

--- Data Base/myiade-inserts.sql
+++ Data Base/myiade-inserts.sql
@@ -1,1 +1,4 @@
-skldmakldmas
+insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('André Santos',to_date('2000-05-21','YYYY.MM.DD'),925478589,'andres@iade.pt','lisboa','11445226', 'M', 32541255); 
+insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Bruna Sousa',to_date('2002-01-02','YYYY.MM.DD'),965544324,'brunas@iade.pt','lisboa','23232335', 'F', 77553844);  
+insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Fábio Santos',to_date('2001-06-20','YYYY.MM.DD'),912239954,'fabios@iade.pt','lisboa','55442356', 'M', 33452252);  
+
