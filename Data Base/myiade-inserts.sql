e00615f0c2967c0354c1270c6938088573f8d0c8
Rúben Martins <91032103+RubenABM@users.noreply.github.com>
2021-11-17 10:21:05 +0000

    Update myiade-inserts.sql

--- Data Base/myiade-inserts.sql
+++ Data Base/myiade-inserts.sql
@@ -2,4 +2,47 @@ insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, st
 insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Bruna Sousa',to_date('2002-01-02','YYYY.MM.DD'),965544324,'brunas@iade.pt','lisboa','23232335', 'F', 77553844);  
 insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Fábio Santos',to_date('2001-06-20','YYYY.MM.DD'),912239954,'fabios@iade.pt','lisboa','55442356', 'M', 33452252);
 insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Beatriz Carreira',to_date('2001-08-20','YYYY.MM.DD'),927712439,'beatrizc@iade.pt','lisboa','11445226', 'F', 77884434); 
-insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Diogo José',to_date('2000-11-02','YYYY.MM.DD'),963733483,'diogoj@iade.pt','lisboa','23232335', 'M' 43453295);
+insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Diogo José',to_date('2000-11-02','YYYY.MM.DD'),963733483,'diogoj@iade.pt','lisboa','23232335', 'M', 43453295);
+
+insert into course (cour_name) values ('Engenharia Informática')
+insert into course (cour_name) values ('Informática de Gestão')
+insert into course (cour_name) values ('Games Development')
+
+insert into unit (unit_name, unit_semester) values ('Análise Matemática I', 1)
+insert into unit (unit_name, unit_semester) values ('Álgebra Linear', 1)
+insert into unit (unit_name, unit_semester) values ('Sistemas Digitais', 1)
+insert into unit (unit_name, unit_semester) values ('Fundamentos da Programação', 1)
+insert into unit (unit_name, unit_semester) values ('Estruturação do Pensamento Lógico', 1)
+insert into unit (unit_name, unit_semester) values ('Metodologia de Investigação', 1)
+insert into unit (unit_name, unit_semester) values ('Análise Matemática II', 2)
+insert into unit (unit_name, unit_semester) values ('Física', 2)
+insert into unit (unit_name, unit_semester) values ('Programação e Algoritmos', 2)
+insert into unit (unit_name, unit_semester) values ('Arquitectura de Computadores', 2)
+insert into unit (unit_name, unit_semester) values ('Redes e Comunicação de Dados', 2)
+insert into unit (unit_name, unit_semester) values ('Matemática Discreta', 3)
+insert into unit (unit_name, unit_semester) values ('Bases de Dados', 3)
+insert into unit (unit_name, unit_semester) values ('Programação de Dispositivos Móveis', 3)
+insert into unit (unit_name, unit_semester) values ('Programação Orientada por Objetos', 3)
+insert into unit (unit_name, unit_semester) values ('Competências Comunicacionais', 3)
+insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento Móvel', 3)
+insert into unit (unit_name, unit_semester) values ('Estatística', 4)
+insert into unit (unit_name, unit_semester) values ('Algoritmos e Estruturas de Dados', 4)
+insert into unit (unit_name, unit_semester) values ('Programação Web', 4)
+insert into unit (unit_name, unit_semester) values ('Interfaces e Usabilidade', 4)
+insert into unit (unit_name, unit_semester) values ('Sistemas de Informação Geográficos', 4)
+insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento Web', 4)
+insert into unit (unit_name, unit_semester) values ('Segurança Informática', 5)
+insert into unit (unit_name, unit_semester) values ('Inteligência Artificial', 5)
+insert into unit (unit_name, unit_semester) values ('Sistemas Distribuídos', 5)
+insert into unit (unit_name, unit_semester) values ('Engenharia de Software', 5)
+insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento de Software', 5)
+insert into unit (unit_name, unit_semester) values ('Computação Física e IoT', 6)
+insert into unit (unit_name, unit_semester) values ('Sistemas Operativos', 6)
+insert into unit (unit_name, unit_semester) values ('Compiladores', 6)
+insert into unit (unit_name, unit_semester) values ('Project Factory', 6)
+insert into unit (unit_name, unit_semester) values ('Ética e Deontologia Profissional', 6)
+insert into unit (unit_name, unit_semester) values ('Empreendedorismo - Base Tecnológica', 6)
+
+
+
+
