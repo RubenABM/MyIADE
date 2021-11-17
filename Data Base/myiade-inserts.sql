1bddea22641356972c8663d7955ca5a912a40294
Rúben Martins <91032103+RubenABM@users.noreply.github.com>
2021-11-17 10:45:20 +0000

    Update myiade-inserts.sql

--- Data Base/myiade-inserts.sql
+++ Data Base/myiade-inserts.sql
@@ -4,112 +4,114 @@ insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, st
 insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Beatriz Carreira',to_date('2001-08-20','YYYY.MM.DD'),927712439,'beatrizc@iade.pt','lisboa','11445226', 'F', 77884434); 
 insert into students (stu_name, stu_bdate, stu_phone, stu_email, stu_address, stu_password, stu_gender, stu_civilnum) values ('Diogo José',to_date('2000-11-02','YYYY.MM.DD'),963733483,'diogoj@iade.pt','lisboa','23232335', 'M', 43453295);
 
-insert into course (cour_name) values ('Engenharia Informática')
-insert into course (cour_name) values ('Informática de Gestão')
-insert into course (cour_name) values ('Games Development')
+insert into course (cour_name) values ('Engenharia Informática');
+insert into course (cour_name) values ('Informática de Gestão');
+insert into course (cour_name) values ('Games Development');
 
-insert into unit (unit_name, unit_semester) values ('Análise Matemática I', 1)
-insert into unit (unit_name, unit_semester) values ('Álgebra Linear', 1)
-insert into unit (unit_name, unit_semester) values ('Sistemas Digitais', 1)
-insert into unit (unit_name, unit_semester) values ('Fundamentos da Programação', 1)
-insert into unit (unit_name, unit_semester) values ('Estruturação do Pensamento Lógico', 1)
-insert into unit (unit_name, unit_semester) values ('Metodologia de Investigação', 1)
-insert into unit (unit_name, unit_semester) values ('Análise Matemática II', 2)
-insert into unit (unit_name, unit_semester) values ('Física', 2)
-insert into unit (unit_name, unit_semester) values ('Programação e Algoritmos', 2)
-insert into unit (unit_name, unit_semester) values ('Arquitectura de Computadores', 2)
-insert into unit (unit_name, unit_semester) values ('Redes e Comunicação de Dados', 2)
-insert into unit (unit_name, unit_semester) values ('Matemática Discreta', 3)
-insert into unit (unit_name, unit_semester) values ('Bases de Dados', 3)
-insert into unit (unit_name, unit_semester) values ('Programação de Dispositivos Móveis', 3)
-insert into unit (unit_name, unit_semester) values ('Programação Orientada por Objetos', 3)
-insert into unit (unit_name, unit_semester) values ('Competências Comunicacionais', 3)
-insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento Móvel', 3)
-insert into unit (unit_name, unit_semester) values ('Estatística', 4)
-insert into unit (unit_name, unit_semester) values ('Algoritmos e Estruturas de Dados', 4)
-insert into unit (unit_name, unit_semester) values ('Programação Web', 4)
-insert into unit (unit_name, unit_semester) values ('Interfaces e Usabilidade', 4)
-insert into unit (unit_name, unit_semester) values ('Sistemas de Informação Geográficos', 4)
-insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento Web', 4)
-insert into unit (unit_name, unit_semester) values ('Segurança Informática', 5)
-insert into unit (unit_name, unit_semester) values ('Inteligência Artificial', 5)
-insert into unit (unit_name, unit_semester) values ('Sistemas Distribuídos', 5)
-insert into unit (unit_name, unit_semester) values ('Engenharia de Software', 5)
-insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento de Software', 5)
-insert into unit (unit_name, unit_semester) values ('Computação Física e IoT', 6)
-insert into unit (unit_name, unit_semester) values ('Sistemas Operativos', 6)
-insert into unit (unit_name, unit_semester) values ('Compiladores', 6)
-insert into unit (unit_name, unit_semester) values ('Project Factory', 6)
-insert into unit (unit_name, unit_semester) values ('Ética e Deontologia Profissional', 6)
-insert into unit (unit_name, unit_semester) values ('Empreendedorismo - Base Tecnológica', 6)
+insert into unit (unit_name, unit_semester) values ('Análise Matemática I', 1);
+insert into unit (unit_name, unit_semester) values ('Álgebra Linear', 1);
+insert into unit (unit_name, unit_semester) values ('Sistemas Digitais', 1);
+insert into unit (unit_name, unit_semester) values ('Fundamentos da Programação', 1);
+insert into unit (unit_name, unit_semester) values ('Estruturação do Pensamento Lógico', 1);
+insert into unit (unit_name, unit_semester) values ('Metodologia de Investigação', 1);
+insert into unit (unit_name, unit_semester) values ('Análise Matemática II', 2);
+insert into unit (unit_name, unit_semester) values ('Física', 2);
+insert into unit (unit_name, unit_semester) values ('Programação e Algoritmos', 2);
+insert into unit (unit_name, unit_semester) values ('Arquitectura de Computadores', 2);
+insert into unit (unit_name, unit_semester) values ('Redes e Comunicação de Dados', 2);
+insert into unit (unit_name, unit_semester) values ('Matemática Discreta', 3);
+insert into unit (unit_name, unit_semester) values ('Bases de Dados', 3);
+insert into unit (unit_name, unit_semester) values ('Programação de Dispositivos Móveis', 3);
+insert into unit (unit_name, unit_semester) values ('Programação Orientada por Objetos', 3);
+insert into unit (unit_name, unit_semester) values ('Competências Comunicacionais', 3);
+insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento Móvel', 3);
+insert into unit (unit_name, unit_semester) values ('Estatística', 4);
+insert into unit (unit_name, unit_semester) values ('Algoritmos e Estruturas de Dados', 4);
+insert into unit (unit_name, unit_semester) values ('Programação Web', 4);
+insert into unit (unit_name, unit_semester) values ('Interfaces e Usabilidade', 4);
+insert into unit (unit_name, unit_semester) values ('Sistemas de Informação Geográficos', 4);
+insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento Web', 4);
+insert into unit (unit_name, unit_semester) values ('Segurança Informática', 5);
+insert into unit (unit_name, unit_semester) values ('Inteligência Artificial', 5);
+insert into unit (unit_name, unit_semester) values ('Sistemas Distribuídos', 5);
+insert into unit (unit_name, unit_semester) values ('Engenharia de Software', 5);
+insert into unit (unit_name, unit_semester) values ('Projeto de Desenvolvimento de Software', 5);
+insert into unit (unit_name, unit_semester) values ('Computação Física e IoT', 6);
+insert into unit (unit_name, unit_semester) values ('Sistemas Operativos', 6);
+insert into unit (unit_name, unit_semester) values ('Compiladores', 6);
+insert into unit (unit_name, unit_semester) values ('Project Factory', 6);
+insert into unit (unit_name, unit_semester) values ('Ética e Deontologia Profissional', 6);
+insert into unit (unit_name, unit_semester) values ('Empreendedorismo - Base Tecnológica', 6);
 
-insert into unit (unit_name, unit_semester) values ('Matemática', 1)
-insert into unit (unit_name, unit_semester) values ('Arquitetura de Sistemas de Informação', 1)
-insert into unit (unit_name, unit_semester) values ('Gestão Empresarial', 1)
-insert into unit (unit_name, unit_semester) values ('Ciência, Fontes e Métodos', 1)
-insert into unit (unit_name, unit_semester) values ('Matemática Discreta', 2)
-insert into unit (unit_name, unit_semester) values ('Investigação Operacional', 2)
-insert into unit (unit_name, unit_semester) values ('Bases de Dados', 2)
-insert into unit (unit_name, unit_semester) values ('Criatividade e Pensamento Crítico', 2)
-insert into unit (unit_name, unit_semester) values ('Competências Comunicacionais', 2)
-insert into unit (unit_name, unit_semester) values ('Programação Orientada a Objetos', 2)
-insert into unit (unit_name, unit_semester) values ('Estatística', 3)
-insert into unit (unit_name, unit_semester) values ('Algoritmos e Estrutura de Dados', 3)
-insert into unit (unit_name, unit_semester) values ('Análise de Sistemas', 3)
-insert into unit (unit_name, unit_semester) values ('Contabilidade', 3)
-insert into unit (unit_name, unit_semester) values ('Design de Interfaces Gráficas', 3)
-insert into unit (unit_name, unit_semester) values ('Desenvolvimento de Interfaces Web', 3)
-insert into unit (unit_name, unit_semester) values ('Cibersegurança', 4)
-insert into unit (unit_name, unit_semester) values ('Redes e Comunicação de Dados	', 4)
-insert into unit (unit_name, unit_semester) values ('Gestão Financeira', 4)
-insert into unit (unit_name, unit_semester) values ('Administração e Gestão de Informação', 4)
-insert into unit (unit_name, unit_semester) values ('Gestão de Recursos Humanos', 5)
-insert into unit (unit_name, unit_semester) values ('Economia', 5)
-insert into unit (unit_name, unit_semester) values ('Business Intelligence', 5)
-insert into unit (unit_name, unit_semester) values ('Sistemas de Informação Geográficos', 5)
-insert into unit (unit_name, unit_semester) values ('Data Mining and Web Analytics', 5)
-insert into unit (unit_name, unit_semester) values ('Projeto de Visualização de Informação', 5)
-insert into unit (unit_name, unit_semester) values ('Marketing e Comunicação Digital', 6)
-insert into unit (unit_name, unit_semester) values ('Business Process Management', 6)
-insert into unit (unit_name, unit_semester) values ('Gestão de Projetos Informáticos', 6)
-insert into unit (unit_name, unit_semester) values ('Empreendedorismo - Base Tecnológica', 6)
-insert into unit (unit_name, unit_semester) values ('Ética e Deontologia Profissional/Voluntariado', 6)
-insert into unit (unit_name, unit_semester) values ('Projeto Final', 6)
+insert into unit (unit_name, unit_semester) values ('Matemática', 1);
+insert into unit (unit_name, unit_semester) values ('Arquitetura de Sistemas de Informação', 1);
+insert into unit (unit_name, unit_semester) values ('Gestão Empresarial', 1);
+insert into unit (unit_name, unit_semester) values ('Ciência, Fontes e Métodos', 1);
+insert into unit (unit_name, unit_semester) values ('Matemática Discreta', 2);
+insert into unit (unit_name, unit_semester) values ('Investigação Operacional', 2);
+insert into unit (unit_name, unit_semester) values ('Bases de Dados', 2);
+insert into unit (unit_name, unit_semester) values ('Criatividade e Pensamento Crítico', 2);
+insert into unit (unit_name, unit_semester) values ('Competências Comunicacionais', 2);
+insert into unit (unit_name, unit_semester) values ('Programação Orientada a Objetos', 2);
+insert into unit (unit_name, unit_semester) values ('Estatística', 3);
+insert into unit (unit_name, unit_semester) values ('Algoritmos e Estrutura de Dados', 3);
+insert into unit (unit_name, unit_semester) values ('Análise de Sistemas', 3);
+insert into unit (unit_name, unit_semester) values ('Contabilidade', 3);
+insert into unit (unit_name, unit_semester) values ('Design de Interfaces Gráficas', 3);
+insert into unit (unit_name, unit_semester) values ('Desenvolvimento de Interfaces Web', 3);
+insert into unit (unit_name, unit_semester) values ('Cibersegurança', 4);
+insert into unit (unit_name, unit_semester) values ('Redes e Comunicação de Dados	', 4);
+insert into unit (unit_name, unit_semester) values ('Gestão Financeira', 4);
+insert into unit (unit_name, unit_semester) values ('Administração e Gestão de Informação', 4);
+insert into unit (unit_name, unit_semester) values ('Gestão de Recursos Humanos', 5);
+insert into unit (unit_name, unit_semester) values ('Economia', 5);
+insert into unit (unit_name, unit_semester) values ('Business Intelligence', 5);
+insert into unit (unit_name, unit_semester) values ('Sistemas de Informação Geográficos', 5);
+insert into unit (unit_name, unit_semester) values ('Data Mining and Web Analytics', 5);
+insert into unit (unit_name, unit_semester) values ('Projeto de Visualização de Informação', 5);
+insert into unit (unit_name, unit_semester) values ('Marketing e Comunicação Digital', 6);
+insert into unit (unit_name, unit_semester) values ('Business Process Management', 6);
+insert into unit (unit_name, unit_semester) values ('Gestão de Projetos Informáticos', 6);
+insert into unit (unit_name, unit_semester) values ('Empreendedorismo - Base Tecnológica', 6);
+insert into unit (unit_name, unit_semester) values ('Ética e Deontologia Profissional/Voluntariado', 6);
+insert into unit (unit_name, unit_semester) values ('Projeto Final', 6);
 
-insert into unit (unit_name, unit_semester) values ('Math, Physics and Games I', 1)
-insert into unit (unit_name, unit_semester) values ('Programming Fundamentals', 1)
-insert into unit (unit_name, unit_semester) values ('Visual Art for Games', 1)
-insert into unit (unit_name, unit_semester) values ('Game Production', 1)
-insert into unit (unit_name, unit_semester) values ('Computational Systems for Games', 1)
-insert into unit (unit_name, unit_semester) values ('2D Project', 1)
-insert into unit (unit_name, unit_semester) values ('Communication Skills	', 1)
-insert into unit (unit_name, unit_semester) values ('Game Design', 2)
-insert into unit (unit_name, unit_semester) values ('Web Programming', 2)
-insert into unit (unit_name, unit_semester) values ('Databases', 2)
-insert into unit (unit_name, unit_semester) values ('Web Project', 2)
-insert into unit (unit_name, unit_semester) values ('Ethics and Professional Deontology', 2)
-insert into unit (unit_name, unit_semester) values ('Math and Physics for Games II', 3)
-insert into unit (unit_name, unit_semester) values ('Mobile Programming', 3)
-insert into unit (unit_name, unit_semester) values ('Game Frameworks', 3)
-insert into unit (unit_name, unit_semester) values ('Data Structures', 3)
-insert into unit (unit_name, unit_semester) values ('Multiplatform Project', 3)
-insert into unit (unit_name, unit_semester) values ('Science, Sources and Methods', 3)
-insert into unit (unit_name, unit_semester) values ('Math, Physics and Games III', 4)
-insert into unit (unit_name, unit_semester) values ('Computer Graphics', 4)
-insert into unit (unit_name, unit_semester) values ('Artificial Intelligence', 4)
-insert into unit (unit_name, unit_semester) values ('Software Development Methodologies', 4)
-insert into unit (unit_name, unit_semester) values ('3D Project', 4)
-insert into unit (unit_name, unit_semester) values ('Creativity and Critical Thinking', 4)
-insert into unit (unit_name, unit_semester) values ('Emerging game technologies', 5)
-insert into unit (unit_name, unit_semester) values ('Distributed Programming', 5)
-insert into unit (unit_name, unit_semester) values ('Security for Games', 5)
-insert into unit (unit_name, unit_semester) values ('Advanced Techniquesin Game Programming', 5)
-insert into unit (unit_name, unit_semester) values ('Multiplayer Project', 5)
-insert into unit (unit_name, unit_semester) values ('Business Model for Games', 5)
-insert into unit (unit_name, unit_semester) values ('Usability and user experience', 6)
-insert into unit (unit_name, unit_semester) values ('Integration with Social Networks and Other Platforms', 6)
-insert into unit (unit_name, unit_semester) values ('Game Engine Development', 6)
-insert into unit (unit_name, unit_semester) values ('Final Project', 6)
+insert into unit (unit_name, unit_semester) values ('Math, Physics and Games I', 1);
+insert into unit (unit_name, unit_semester) values ('Programming Fundamentals', 1);
+insert into unit (unit_name, unit_semester) values ('Visual Art for Games', 1);
+insert into unit (unit_name, unit_semester) values ('Game Production', 1);
+insert into unit (unit_name, unit_semester) values ('Computational Systems for Games', 1);
+insert into unit (unit_name, unit_semester) values ('2D Project', 1);
+insert into unit (unit_name, unit_semester) values ('Communication Skills', 1);
+insert into unit (unit_name, unit_semester) values ('Game Design', 2);
+insert into unit (unit_name, unit_semester) values ('Web Programming', 2);
+insert into unit (unit_name, unit_semester) values ('Databases', 2);
+insert into unit (unit_name, unit_semester) values ('Web Project', 2);
+insert into unit (unit_name, unit_semester) values ('Ethics and Professional Deontology', 2);
+insert into unit (unit_name, unit_semester) values ('Math and Physics for Games II', 3);
+insert into unit (unit_name, unit_semester) values ('Mobile Programming', 3);
+insert into unit (unit_name, unit_semester) values ('Game Frameworks', 3);
+insert into unit (unit_name, unit_semester) values ('Data Structures', 3);
+insert into unit (unit_name, unit_semester) values ('Multiplatform Project', 3);
+insert into unit (unit_name, unit_semester) values ('Science, Sources and Methods', 3);
+insert into unit (unit_name, unit_semester) values ('Math, Physics and Games III', 4);
+insert into unit (unit_name, unit_semester) values ('Computer Graphics', 4);
+insert into unit (unit_name, unit_semester) values ('Artificial Intelligence', 4);
+insert into unit (unit_name, unit_semester) values ('Software Development Methodologies', 4);
+insert into unit (unit_name, unit_semester) values ('3D Project', 4);
+insert into unit (unit_name, unit_semester) values ('Creativity and Critical Thinking', 4);
+insert into unit (unit_name, unit_semester) values ('Emerging game technologies', 5);
+insert into unit (unit_name, unit_semester) values ('Distributed Programming', 5);
+insert into unit (unit_name, unit_semester) values ('Security for Games', 5);
+insert into unit (unit_name, unit_semester) values ('Advanced Techniquesin Game Programming', 5);
+insert into unit (unit_name, unit_semester) values ('Multiplayer Project', 5);
+insert into unit (unit_name, unit_semester) values ('Business Model for Games', 5);
+insert into unit (unit_name, unit_semester) values ('Usability and user experience', 6);
+insert into unit (unit_name, unit_semester) values ('Integration with Social Networks and Other Platforms', 6);
+insert into unit (unit_name, unit_semester) values ('Game Engine Development', 6);
+insert into unit (unit_name, unit_semester) values ('Final Project', 6);
+
+commit;
 
 
 
