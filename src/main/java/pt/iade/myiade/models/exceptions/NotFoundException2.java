c33a17610d3c6efecba3bdeefd038c104791ea86
RubenABM <rubenbatistamartins@gmail.com>
2021-11-18 09:28:55 +0000

    notfound2

--- src/main/java/pt/iade/myiade/models/exceptions/NotFoundException2.java
+++ src/main/java/pt/iade/myiade/models/exceptions/NotFoundException2.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class NotFoundException2 extends RuntimeException {
  private static final long serialVersionUID = 5405519104069955535L;
  public NotFoundException2(String email, String elemType, String idName, String password) {
- super(elemType+" with "+ idName + " " + email + " and " + password +  "not found.");
+ super(elemType+" with "+ idName + " " + email + " and password " + password + " not found.");
  }
 }
