8f4828db2445725665f0e52f60e1a9117720d36a
RubenABM <rubenbatistamartins@gmail.com>
2021-11-27 19:00:37 +0000

    units

--- src/main/java/pt/iade/myiade/models/repositories/UnitRepository.java
+++ src/main/java/pt/iade/myiade/models/repositories/UnitRepository.java
@@ -5,5 +5,9 @@ import pt.iade.myiade.models.Unit;
 
 public interface UnitRepository extends CrudRepository<Unit,Integer> 
 {
+    String QueryFindUnitsCourse = 
+    "select cour_name courseName, unit_name unitName from unitcourses "+
+    "inner join units on unitcour_unit_id = unit_id "+
+    "inner join courses on unitcour_cour_id = cour_id";
     
 }
