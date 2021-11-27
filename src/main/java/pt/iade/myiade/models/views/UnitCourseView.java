75e685b390a822478c325837011f548e62ddeab8
RubenABM <rubenbatistamartins@gmail.com>
2021-11-27 17:17:37 +0000

    123

---
+++ src/main/java/pt/iade/myiade/models/views/UnitCourseView.java
@@ -0,0 +1,11 @@
+package pt.iade.myiade.models.views;
+
+public interface UnitCourseView {
+    String getCourseName();
+    int getCourseId();
+    int getSemester();
+    int getId();
+    String getName();
+    int getCredits();
+    }
+    
