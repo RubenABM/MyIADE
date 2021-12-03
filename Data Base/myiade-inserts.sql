f437c745b7062a151e6a905324af93a1820322a3
Rúben Martins <91032103+RubenABM@users.noreply.github.com>
2021-12-03 19:40:15 +0000

    Update myiade-inserts.sql

--- Data Base/myiade-inserts.sql
+++ Data Base/myiade-inserts.sql
@@ -478,6 +478,114 @@ insert into teachers (teach_name, teach_phone, teach_email) values ('Paulo Chave
 insert into teachers (teach_name, teach_phone, teach_email) values ('Joaquim Trovão','923985463','joaquimtt@iade.pt');
 insert into teachers (teach_name, teach_phone, teach_email) values ('Joana Marques','914894321','joanatm@iade.pt');
 
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (1,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (2,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (3,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (4,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (5,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (6,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (7,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (8,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (9,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (10,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (11,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (12,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (13,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (14,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (15,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (16,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (17,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (18,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (19,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (20,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (21,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (22,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (23,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (24,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (25,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (26,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (27,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (28,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (29,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (30,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (31,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (32,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (33,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (34,);
+
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (35,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (36,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (37,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (38,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (39,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (40,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (41,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (42,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (43,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (44,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (45,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (46,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (47,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (49,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (50,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (51,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (52,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (53,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (54,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (55,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (56,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (57,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (58,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (59,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (60,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (61,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (62,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (63,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (64,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (65,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (66,);
+
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (67,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (68,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (69,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (70,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (71,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (72,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (73,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (74,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (75,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (76,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (77,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (78,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (79,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (80,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (81,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (82,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (83,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (84,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (85,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (86,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (87,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (88,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (89,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (90,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (91,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (92,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (93,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (94,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (95,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (96,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (97,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (98,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (99,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (100,);
+
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (101,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (102,);
+insert into uniteachers (unitte_unitcour_id, unitte_teach_id) values (103,);
+
+
+
 
 
 
