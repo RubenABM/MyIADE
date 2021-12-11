9c21bc6301844ae4231796058a70f83eed767929
RubenABM <rubenbatistamartins@gmail.com>
2021-12-11 20:10:48 +0000

    111

--- src/main/java/pt/iade/myiade/controllers/PresenceController.java
+++ src/main/java/pt/iade/myiade/controllers/PresenceController.java
@@ -1,24 +1,16 @@
 package pt.iade.myiade.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.MediaType;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-//import pt.iade.myiade.models.Presence;
-import pt.iade.myiade.models.repositories.PresenceRepository;
-import pt.iade.myiade.models.responses.Response;
-
 import java.time.LocalDate;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pt.iade.myiade.models.repositories.PresenceRepository;
+import pt.iade.myiade.models.responses.Response;
 
 @RestController
 @RequestMapping(path = "/api/presences")
