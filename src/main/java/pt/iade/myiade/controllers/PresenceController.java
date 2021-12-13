798707e1d335339e4fee195d5ba6b7aa1ba8a097
RubenABM <rubenbatistamartins@gmail.com>
2021-12-13 09:43:29 +0000

    posts

--- src/main/java/pt/iade/myiade/controllers/PresenceController.java
+++ src/main/java/pt/iade/myiade/controllers/PresenceController.java
@@ -3,12 +3,16 @@ package pt.iade.myiade.controllers;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import pt.iade.myiade.models.Presence;
 import pt.iade.myiade.models.repositories.PresenceRepository;
 import pt.iade.myiade.models.responses.Response;
 
@@ -26,5 +30,13 @@ public class PresenceController{
 
         Integer inserted = presenceRepository.registerPresence(presenceScheduleID, presenceStudentID, LocalDate.now());
         return new Response(" presence registered -> ", inserted);
+    }
+
+    @PostMapping(path= "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Presence saveUser(@RequestBody Presence presence)
+    {
+        Presence savedPresence = presenceRepository.save(presence);
+        logger.info("Saving user with id " + savedPresence.getId());
+        return savedPresence;
     }
 }
